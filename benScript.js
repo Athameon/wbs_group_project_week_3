2e00e5bb198863c0332d69d68682170005ec9374
Athameon <Benjamin.Nopper@freenet.de>
2021-05-11 08:43:14 +0200

    Created the create of a new toto task.

---
+++ benScript.js
@@ -0,0 +1,71 @@
+const toDoList = document.getElementById("todo");
+const toDoListItems = toDoList.getElementsByClassName("item");
+
+
+document.getElementById("newItem").addEventListener("keyup", function(event) {
+  if (event.key === 'Enter') {
+    createNewTask();
+  }
+}); 
+
+function createNewTask(inputValue = document.getElementById("newItem").value) {
+  if(inputValue === '') {
+    alert('The todo task must be named!');
+    return;
+  }
+
+  const toDoTaskElement = document.createElement("div");
+
+  const checkboxAndTitleSide = createCheckboxAndTitle(inputValue);
+  toDoTaskElement.appendChild(checkboxAndTitleSide);
+  const iconSide = createRightSideIcons();
+  toDoTaskElement.appendChild(iconSide);
+
+  toDoTaskElement.setAttribute("class", "item");
+  toDoTaskElement.setAttribute("id", uuidv4());
+  toDoList.appendChild(toDoTaskElement);
+  console.log(toDoTaskElement);
+}
+
+function createCheckboxAndTitle(inputValue) {
+  const checkboxAndTitleSide = document.createElement("div");
+
+  document.getElementById("newItem").value = '';
+
+  const checkboxElement = document.createElement("INPUT");
+  checkboxElement.setAttribute("type", "checkbox");
+  checkboxAndTitleSide.appendChild(checkboxElement);
+
+  const spans = document.createElement("span");
+  spans.innerHTML = inputValue;
+  checkboxAndTitleSide.appendChild(spans);
+
+  const editTitleElement = document.createElement("INPUT");
+  editTitleElement.setAttribute("type", "text");
+  editTitleElement.setAttribute("value", inputValue);
+  editTitleElement.setAttribute("class", "edit");
+  checkboxAndTitleSide.appendChild(editTitleElement);
+
+  return checkboxAndTitleSide;
+}
+
+function createRightSideIcons() {
+  const iconSide = document.createElement("div");
+
+  iconSide.setAttribute("class", "itemItems");
+  const editElement = document.createElement("i");
+  editElement.setAttribute("class", "fas fa-pen fa-2x");
+  iconSide.appendChild(editElement);
+  const deleteElement = document.createElement("i");
+  deleteElement.setAttribute("class", "fas fa-trash-alt fa-2x");
+  iconSide.appendChild(deleteElement);
+
+  return iconSide;
+}
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
