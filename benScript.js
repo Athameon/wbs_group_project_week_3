0370433d656a449f4b469d0ddb6b853f17345a05
Athameon <Benjamin.Nopper@freenet.de>
2021-05-11 12:13:06 +0200

    Fixed bug not calling the createNewTask methot on the button click. Changed implementation of the createNewTask function.

--- benScript.js
+++ benScript.js
@@ -9,59 +9,83 @@ document.getElementById("newItem").addEventListener("keyup", function(event) {
 }); 
 
 function createNewTask(inputValue = document.getElementById("newItem").value) {
+  console.log("Called createNewTask");
   if(inputValue === '') {
     alert('The todo task must be named!');
     return;
   }
 
-  const toDoTaskElement = document.createElement("div");
-
-  const checkboxAndTitleSide = createCheckboxAndTitle(inputValue);
-  toDoTaskElement.appendChild(checkboxAndTitleSide);
-  const iconSide = createRightSideIcons();
-  toDoTaskElement.appendChild(iconSide);
-
-  toDoTaskElement.setAttribute("class", "item");
-  toDoTaskElement.setAttribute("id", uuidv4());
-  toDoList.appendChild(toDoTaskElement);
-  console.log(toDoTaskElement);
+  const taskTemplate = `
+    <div class="item" id="${uuidv4()}">
+      <div>
+        <input type="checkbox">
+        <span>${inputValue}</span>
+        <input class="edit" type="text" value="${inputValue}">
+      </div>
+      <div class="itemItems">
+        <i class="fas fa-pen fa-2x"></i>
+        <i class="fas fa-trash-alt fa-2x"></i>
+      </div>
+    </div>
+    `
+    toDoList.insertAdjacentHTML("beforeend", taskTemplate);
+    document.getElementById("newItem").value = '';
 }
 
-function createCheckboxAndTitle(inputValue) {
-  const checkboxAndTitleSide = document.createElement("div");
+// function createNewTask(inputValue = document.getElementById("newItem").value) {
+//   if(inputValue === '') {
+//     alert('The todo task must be named!');
+//     return;
+//   }
 
-  document.getElementById("newItem").value = '';
+//   const toDoTaskElement = document.createElement("div");
 
-  const checkboxElement = document.createElement("INPUT");
-  checkboxElement.setAttribute("type", "checkbox");
-  checkboxAndTitleSide.appendChild(checkboxElement);
+//   const checkboxAndTitleSide = createCheckboxAndTitle(inputValue);
+//   toDoTaskElement.appendChild(checkboxAndTitleSide);
+//   const iconSide = createRightSideIcons();
+//   toDoTaskElement.appendChild(iconSide);
 
-  const spans = document.createElement("span");
-  spans.innerHTML = inputValue;
-  checkboxAndTitleSide.appendChild(spans);
+//   toDoTaskElement.setAttribute("class", "item");
+//   toDoTaskElement.setAttribute("id", uuidv4());
+//   toDoList.appendChild(toDoTaskElement);
+//   console.log(toDoTaskElement);
+// }
 
-  const editTitleElement = document.createElement("INPUT");
-  editTitleElement.setAttribute("type", "text");
-  editTitleElement.setAttribute("value", inputValue);
-  editTitleElement.setAttribute("class", "edit");
-  checkboxAndTitleSide.appendChild(editTitleElement);
+// function createCheckboxAndTitle(inputValue) {
+//   const checkboxAndTitleSide = document.createElement("div");
 
-  return checkboxAndTitleSide;
-}
+//   document.getElementById("newItem").value = '';
 
-function createRightSideIcons() {
-  const iconSide = document.createElement("div");
+//   const checkboxElement = document.createElement("INPUT");
+//   checkboxElement.setAttribute("type", "checkbox");
+//   checkboxAndTitleSide.appendChild(checkboxElement);
 
-  iconSide.setAttribute("class", "itemItems");
-  const editElement = document.createElement("i");
-  editElement.setAttribute("class", "fas fa-pen fa-2x");
-  iconSide.appendChild(editElement);
-  const deleteElement = document.createElement("i");
-  deleteElement.setAttribute("class", "fas fa-trash-alt fa-2x");
-  iconSide.appendChild(deleteElement);
+//   const spans = document.createElement("span");
+//   spans.textContent = inputValue;
+//   checkboxAndTitleSide.appendChild(spans);
 
-  return iconSide;
-}
+//   const editTitleElement = document.createElement("INPUT");
+//   editTitleElement.setAttribute("type", "text");
+//   editTitleElement.setAttribute("value", inputValue);
+//   editTitleElement.setAttribute("class", "edit");
+//   checkboxAndTitleSide.appendChild(editTitleElement);
+
+//   return checkboxAndTitleSide;
+// }
+
+// function createRightSideIcons() {
+//   const iconSide = document.createElement("div");
+
+//   iconSide.setAttribute("class", "itemItems");
+//   const editElement = document.createElement("i");
+//   editElement.setAttribute("class", "fas fa-pen fa-2x");
+//   iconSide.appendChild(editElement);
+//   const deleteElement = document.createElement("i");
+//   deleteElement.setAttribute("class", "fas fa-trash-alt fa-2x");
+//   iconSide.appendChild(deleteElement);
+
+//   return iconSide;
+// }
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
